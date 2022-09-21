f4636136b844d6445fa5146fb8729458a518bb8c
Joakim Gottzén <joakim.gottzen@gmail.com>
2022-09-21 18:34:31 +0200

    luv: make sure handles are closed

--- lua/ya-tree/debounce.lua
+++ lua/ya-tree/debounce.lua
@@ -7,12 +7,16 @@ local M = {}
 ---@field is_active fun(self: uv_timer_t): boolean?
 ---@field is_closing fun(self: uv_timer_t): boolean?
 
+---@type uv_timer_t[]
+local timers = {}
+
 ---@param fn fun(...)
 ---@param ms number
 ---@return fun(...)
 function M.debounce_trailing(fn, ms)
   ---@type uv_timer_t
   local timer = vim.loop.new_timer()
+  timers[#timers + 1] = timer
   return function(...)
     local args = { ... }
     timer:start(ms, 0, function()
@@ -20,6 +24,18 @@ function M.debounce_trailing(fn, ms)
       vim.schedule_wrap(fn)(unpack(args))
     end)
   end
+end
+
+do
+  local events = require("ya-tree.events")
+  local event = require("ya-tree.events.event").autocmd
+  events.on_autocmd_event(event.LEAVE_PRE, "YA_TREE_DEBOUNCE_CLEANUP", function()
+    for _, timer in ipairs(timers) do
+      if not timer:is_closing() then
+        timer:close()
+      end
+    end
+  end)
 end
 
 return M
