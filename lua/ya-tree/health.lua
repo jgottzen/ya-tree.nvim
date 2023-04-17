d729f5a21e589a3450c2c7d14bf4638cbe5a98e1
Joakim Gottzén <joakim.gottzen@gmail.com>
2023-04-17 12:57:41 +0200

    checkhealth: handle deprecated functions in nvim 0.11

--- lua/ya-tree/health.lua
+++ lua/ya-tree/health.lua
@@ -1,75 +1,78 @@
 local M = {}
 
 function M.check()
-  local start, ok, warn, report_error = vim.health.report_start, vim.health.report_ok, vim.health.report_warn, vim.health.report_error
+  local _start = vim.health.start or vim.health.report_start
+  local _ok = vim.health.ok or vim.health.report_ok
+  local _warn = vim.health.warn or vim.health.report_warn
+  local _error = vim.health.error or vim.health.report_error
 
   if vim.fn.has("nvim-0.8.0") == 1 then
-    ok("Neovim version >= 0.8.0")
+    _ok("Neovim version >= 0.8.0")
   else
-    report_error("Neovim version < 0.8.0")
+    _error("Neovim version < 0.8.0")
   end
 
-  start("Checking for required plugins")
+  _start("Checking for required plugins")
   local nui = pcall(require, "nui.input")
   if nui then
-    ok("**plugins:** `nui.nvim` installed")
+    _ok("**plugins:** `nui.nvim` installed")
   else
-    report_error("**plugins:** `nui.nvim` not installed")
+    _error("**plugins:** `nui.nvim` not installed")
   end
 
-  start("Checking for optional plugins")
+  _start("Checking for optional plugins")
   local devicons = pcall(require, "nvim-web-devicons")
   if devicons then
-    ok("**plugins:** `nvim-web-devicons` installed")
+    _ok("**plugins:** `nvim-web-devicons` installed")
   else
-    warn("**plugins:** `nvim-web-devicons` not installed")
+    _warn("**plugins:** `nvim-web-devicons` not installed")
   end
 
-  start("Checking for executables")
+  _start("Checking for executables")
   local git, message = pcall(vim.fn.systemlist, { "git", "--version" })
   if git then
     local version = vim.split(message[1], " ", { plain = true })[3]
-    ok("**git:** `git` version " .. version)
+    _ok("**git:** `git` version " .. version)
     if vim.fn.executable("yadm") == 1 then
-      ok("**yadm:** `yadm` executable found")
+      _ok("**yadm:** `yadm` executable found")
     else
-      warn("**yadm**: no executable found")
+      _warn("**yadm**: no executable found")
     end
   else
-    warn("**git:** no `git` executable found")
+    _warn("**git:** no `git` executable found")
   end
 
   if vim.fn.executable("fd") == 1 then
-    ok("**search:** `fd` executable found")
+    _ok("**search:** `fd` executable found")
   elseif vim.fn.executable("fdfind") == 1 then
-    ok("**search:** `fdfind` executable found")
+    _ok("**search:** `fdfind` executable found")
   elseif vim.fn.executable("find") == 1 and vim.fn.has("win32") == 0 then
-    ok("**search:** `find` executable found")
+    _ok("**search:** `find` executable found")
   elseif vim.fn.executable("where") == 1 then
-    ok("**search:** `where` executable found")
+    _ok("**search:** `where` executable found")
   else
-    warn("**search:** no executable found")
+    _warn("**search:** no executable found")
   end
 
   if vim.fn.executable("trash") == 1 then
-    ok("**trash:** `trash` executable found")
+    _ok("**trash:** `trash` executable found")
   else
-    warn("**trash:** no executable found")
+    _warn("**trash:** no executable found")
   end
 
-  start("Checking configuration")
+  _start("Checking configuration")
   local config_warnings = false
   if not require("ya-tree.config").setup_called then
-    report_error("**config:** `ya-tree.setup()` has not been called")
+    _error("**config:** `ya-tree.setup()` has not been called")
     config_warnings = true
   end
   local config = require("ya-tree.config").config
   if config.search.max_results == 0 then
-    warn("**config:** 'conifg.search.max_results' is set to `0`, this can cause performance problems")
+    _warn("**config:** 'conifg.search.max_results' is set to `0`, this can cause performance problems")
     config_warnings = true
   end
   if not config_warnings then
-    ok("**config:** no issues found")
+    _ok("**config:** no issues found")
   end
 end
 
